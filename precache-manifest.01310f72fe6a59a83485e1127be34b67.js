66fc34bf80f9460ac3ebdd85129f06402d0ab6dd
Diego Chagas <drochachagas@gmail.com>
2019-12-16 20:52:25 -0300

    Updates

---
+++ precache-manifest.01310f72fe6a59a83485e1127be34b67.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91a03c5d11d2719ac6464ebbae28ad6a",
+    "url": "/weather-api/index.html"
+  },
+  {
+    "revision": "687aa8570cd709cdfa91",
+    "url": "/weather-api/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "cf9698711e7732739f36",
+    "url": "/weather-api/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "687aa8570cd709cdfa91",
+    "url": "/weather-api/static/js/2.9e971d85.chunk.js"
+  },
+  {
+    "revision": "4c587c1a353aebc8ddd915a8bd1861c1",
+    "url": "/weather-api/static/js/2.9e971d85.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cf9698711e7732739f36",
+    "url": "/weather-api/static/js/main.3b58d71b.chunk.js"
+  },
+  {
+    "revision": "973a8fb5b08a59928e7e",
+    "url": "/weather-api/static/js/runtime-main.51249204.js"
+  }
+]);
